357de60148c6d6089fc76eab5d45ab601a339027
ahadullabaig <ahadullabaig.16@gmail.com>
2026-01-21 23:31:07 +0530

    Part A - Q1

---
+++ part_a/Employee.java
@@ -0,0 +1,61 @@
+import java.util.Scanner;
+
+public class Employee
+{
+    String id;
+    String name;
+    int age;
+    int salary;
+
+    void accept()
+    {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("\nEnter ID: ");
+        id = sc.next();
+
+        System.out.print("Enter Name: ");
+        name = sc.next();
+
+        System.out.print("Enter Age: ");
+        age = sc.nextInt();
+
+        System.out.print("Enter Salary: ");
+        salary = sc.nextInt();
+    }
+
+    public static void main(String[] args)
+    {
+        Employee[] employees = new Employee[3];
+
+        for(int i=0; i<3; i++)
+        {
+            employees[i] = new Employee();
+
+            System.out.println("Enter details for Employee " + (i + 1) + ":");
+
+            employees[i].accept();
+        }
+
+        // Sort employees by salary in ascending order
+        for (int i = 0; i < employees.length - 1; i++)
+        {
+            for (int j = 0; j < employees.length - 1 - i; j++)
+            {
+                if (employees[j].salary > employees[j + 1].salary)
+                {
+                    Employee temp = employees[j];
+                    employees[j] = employees[j + 1];
+                    employees[j + 1] = temp;
+                }
+            }
+        }
+
+        System.out.println("\nNames in order of salary:");
+
+        for(Employee e : employees)
+        {
+            System.out.println(e.name);
+        }
+    }
+}
