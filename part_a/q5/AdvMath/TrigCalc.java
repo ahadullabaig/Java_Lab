79ad9f0be8d45d4993a62bb8bec03c0abd89655f
ahadullabaig <ahadullabaig.16@gmail.com>
2026-01-22 22:59:23 +0530

    Part A - Q5

---
+++ part_a/q5/AdvMath/TrigCalc.java
@@ -0,0 +1,10 @@
+package AdvMath;
+
+public class TrigCalc
+{
+    public double calculate(double x)
+    {
+        return Math.tan(x) + Math.cos(x) + Math.sin(x);
+    }
+}
+
---
+++ part_a/q5/Calculate.java
@@ -0,0 +1,12 @@
+import AdvMath.TrigCalc;
+
+public class Calculate
+{
+    public static void main(String[]args)
+    {
+        TrigCalc c = new TrigCalc();
+        
+        System.out.println("y = " + c.calculate(10.10));
+    }
+}
+
